b837ccb28a4d96db0f141f368d5c5ebb22dd34c0
asrivastava <asrivastava@mitre.org>
2021-12-23 14:05:59 -0500

    add empty test for internal functions

---
+++ tests/molten_tests/test_internal_functions.py
@@ -0,0 +1,10 @@
+""" Making an example file. These functions may test internal common portions of the code.
+    Examples include:
+    - Does LLH work as intended (can try on tiny list rather than realistic one)
+    - Does the general flow of a Detector work correctly (e.g., what happens when drift is detected, etc.)
+    In general this is left about the detectors detecting and more about them working. 
+    Also I needed a new file to test changes in the VM GitLab Runner.
+"""
+
+def test_internal_function():
+    assert True
