914ff6b80ff933557b550fc367b60130f0d199a0
India G Lindsay <ilindsay@mitre.org>
2022-04-15 16:30:39 -0400

    Hdddm cbdb

---
+++ examples/Batch/HDDDM.py
@@ -0,0 +1,122 @@
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from molten.data_drift.hdddm import HDDDM
+
+
+""" 
+
+HDDDM Example
+
+This file details how to setup, run, and produce plots for HDDDM, using both
+numeric and categorical data. Drift occurs in 2009, 2012, 2015, 2018, and 2021.
+Drift in 2018 persists through 2021. See
+src/molten/tools/artifacts/README_example_data.txt for more info.
+
+HDDDM must be setup and run with batches of data. 
+
+Plots include:
+- A line plot visualizing test statistics for detection of drift 
+
+- A heatmap visualizing "where" drift is occuring, showing features for each
+  year's test batch with the greatest Hellinger distance from the reference
+  batch.
+"""
+
+## Setup ##
+
+# Import data
+data = pd.read_csv(
+    os.path.join("src", "molten", "tools", "artifacts", "example_data.csv"),
+    index_col="id",
+)
+
+# Set up reference and test batches, using 2007 as reference year
+# -2 indexing removes columns "drift" and "confidence"
+reference = data[data.year == 2007].iloc[:, 1:-2]
+all_test = data[data.year != 2007]
+
+#  Run HDDDM
+hdddm = HDDDM(reference, subsets=8)
+# Store epsilons per feature for heatmap
+feature_epsilons = []
+# Store drift for test statistic plot
+detected_drift = []
+for year, subset_data in all_test.groupby("year"):
+    hdddm.update(subset_data.iloc[:, 1:-2])
+    feature_epsilons.append(hdddm.feature_epsilons)
+    detected_drift.append(hdddm.drift_state)
+
+## Plot Line Graph ##
+
+# HDDDM identifies drifts in 2009, 2010, 2012, 2019, 2021. These drifts involve
+# a change in mean or variance. Drift in 2010 is likely identified as the
+# distribution returns to state prior tio 2009 drift. Drift in 2015, a change in
+# correlation, is undetected. Drift in 2018 is detected one year late.
+
+years = list(data.year.value_counts().index[1:])
+h_distances = [
+    ep - th for ep, th in zip(hdddm.epsilon_values.values(), hdddm.thresholds.values())
+]
+plot_data = pd.DataFrame(
+    {"Year": years, "Hellinger Distance": h_distances, "Detected Drift": detected_drift}
+)
+sns.set_style("white")
+plt.figure(figsize=(20, 6))
+plt.plot(
+    "Year", "Hellinger Distance", data=plot_data, label="Hellinger Distance", marker="."
+)
+plt.grid(False, axis="x")
+plt.xticks(years, fontsize=16)
+plt.yticks(fontsize=16)
+plt.title("HDDDM", fontsize=22)
+plt.ylabel("Hellinger Distance", fontsize=18)
+plt.xlabel("Year", fontsize=18)
+plt.ylim([min(h_distances) - 0.02, max(h_distances) + 0.02])
+for _, t in enumerate(plot_data.loc[plot_data["Detected Drift"] == "drift"]["Year"]):
+    plt.axvspan(
+        t - 0.2, t + 0.2, alpha=0.5, color="red", label=("Drift" if _ == 0 else None)
+    )
+plt.legend()
+plt.axhline(y=0, color="orange", linestyle="dashed")
+plt.savefig("HDDDM_test_statistics.png")
+
+## Plot Heatmap ##
+
+# Drift in feature B is detected in 2009 and 2010 (as it reverts to normal).
+# Drift in feature D is detected in 2012 and 2013 (as it reverts to normal).
+# Drift in feature H is detected in 2019. Drift in feature J is detected in 2021.
+# The undetected drift occurs in 2015 in the correlations between features E and F.
+
+sns.set_style("whitegrid")
+sns.set(rc={"figure.figsize": (15, 8)})
+
+# flip axis of feature epsilon matrix
+inverted_matrix = []
+for alpha in range(0, len(list(data.columns[1:-2]))):
+    var = []
+    for year in range(len(feature_epsilons)):
+        var.append(feature_epsilons[year][alpha])
+    inverted_matrix.append(var)
+
+
+# Setup plot
+grid_kws = {"height_ratios": (0.9, 0.05), "hspace": 0.3}
+f, (ax, cbar_ax) = plt.subplots(2, gridspec_kw=grid_kws)
+coloring = sns.cubehelix_palette(start=0.8, rot=-0.5, as_cmap=True)
+ax = sns.heatmap(
+    inverted_matrix,
+    ax=ax,
+    cmap=coloring,
+    xticklabels=years,
+    yticklabels=list(data.columns[1:-2]),
+    linewidths=0.5,
+    cbar_ax=cbar_ax,
+    cbar_kws={"orientation": "horizontal"},
+)
+ax.set(xlabel="Years", ylabel="Features")
+ax.collections[0].colorbar.set_label("Difference in Hellinger Distance")
+ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+# plt.show()
+plt.savefig("HDDDM_feature_heatmap.png")
--- src/molten/tools/artifacts/README_example_data.txt
+++ src/molten/tools/artifacts/README_example_data.txt
@@ -1,16 +1,18 @@
-2022-03-03
+2022-04-15
 
 README for example_data.csv.
 Generated from make_example_data.R.
 
 Covers years 2007 through 2021, each year has 20,000 observations
-Items B, E, and F are normally distributed, all others are gamma
+Features B, E, and F are normally distributed; A, C, D, G, H, I, J are gamma.
+Feature cat contains categorical variables ranging from 1-7 sampled with varying probability
+Feature confidence contains integers ranging from (0,0.6)
 
-Drift 1 (2009): Item B changes in mean but not variance
-Drift 2 (2012): Item D changes in just variance (mean stays same)
-Drift 3 (2015): Item E and F become more strongly correlated. Their mean and variance stay the same
-Drift 4 (2018 - 2021): Item H changes in mean and variance
-Drift 5 (2021): Item J change mean and variance 
+Drift 1 (2009): B changes in mean but not variance
+Drift 2 (2012): D changes in just variance (mean stays same)
+Drift 3 (2015): E and F become more strongly correlated. Their mean and variance stay the same
+Drift 4 (2018 - 2021): H changes in mean and variance, range of Confidence Scores increases to (0.4,1)
+Drift 5 (2021): J change mean and variance 
 
 With the except of drift 4, all instances of drift are only for that single year of data. 
 
