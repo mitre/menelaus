d85c5dcb68b0b23b9f49134f1fa2b3982f4a7d9f
Thomas Schill <33845624+tms-bananaquit@users.noreply.github.com>
2022-06-29 12:35:55 -0400

    remove extra notebooks, fix plotly plots

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,6 +78,11 @@ html_css_files = [
     "custom.css",
 ]
 
+html_js_files = [
+    "require.min.js",  # Add to your _static
+    "custom.js",
+]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
