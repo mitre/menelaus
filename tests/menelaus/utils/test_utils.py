5c38c343c76cdbfeb867b70d9dedff67d3b50e9b
Thomas Schill <33845624+tms-bananaquit@users.noreply.github.com>
2022-07-22 13:19:35 -0400

    make garbage for unit test more garbagey

--- tests/menelaus/utils/test_utils.py
+++ tests/menelaus/utils/test_utils.py
@@ -3,5 +3,5 @@ from menelaus.utils._locate import find_git_root
 
 
 def test_find_root_dir():
-    assert find_git_root("garbage") is None
+    assert find_git_root("garbage_directory_0978697834703245345") is None
     assert ".git" in os.listdir(find_git_root())
