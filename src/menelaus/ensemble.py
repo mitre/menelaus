46a65a29c58636be74b1514221450eb6fb9800ad
Anmol-Srivastava <anmol.srivastava.ca@gmail.com>
2022-08-11 10:00:44 -0400

    add initial draft of StreamingEnsemble, move set_reference into BatchEnsemble

--- src/menelaus/ensemble.py
+++ src/menelaus/ensemble.py
@@ -1,4 +1,4 @@
-from menelaus.drift_detector import BatchDetector
+from menelaus.drift_detector import BatchDetector, StreamingDetector
 
 
 def eval_simple_majority(detectors):
@@ -26,7 +26,7 @@ def eval_simple_majority(detectors):
 # Keeping all evaluator functions in a table enables
 # ensembles to look up via str value, rather than
 # needing a function passed in.
-evaluators = {"simple-majority": eval_simple_majority}
+EVALUATORS = {"simple-majority": eval_simple_majority}
 
 
 class Ensemble:
@@ -42,25 +42,8 @@ class Ensemble:
 
     def __init__(self, detectors: dict, evaluator: str, columns: dict = None):
         self.detectors = detectors.copy()
-        self.evaluator = evaluators[evaluator]
+        self.evaluator = EVALUATORS[evaluator]
         self.columns = columns
-
-    def set_reference(self, X, y_true, y_pred):
-        """
-        Initialize each detector in ensemble with a reference batch.
-
-        Args:
-            X (pandas.DataFrame or numpy.array): baseline dataset
-            y_true (numpy.array): actual labels of dataset
-            y_pred (numpy.array): predicted labels of dataset
-        """
-        for det_key in self.detectors:
-            # XXX - Cannot re-define X = constrain(), else external reference is modified
-            #       Need to see why this is happening and where to put e.g. a copy() stmt.
-            X_selected = self.select_data(X, det_key)
-            self.detectors[det_key].set_reference(
-                X=X_selected, y_true=y_true, y_pred=y_pred
-            )
 
     def update(self, X, y_true=None, y_pred=None):
         """
@@ -119,6 +102,55 @@ class Ensemble:
             self.detectors[det_key].reset()
 
 
+class StreamingEnsemble(StreamingDetector, Ensemble):
+    """
+    Implements Ensemble class for streaming drift detectors. Inherits
+    from Ensemble and StreamingDetector (i.e., StreamingEnsemble IS-A StreamingDetector).
+    As such it has the functions of a regular detector: update, and reset. 
+    Internally, these operate not only on the ensemble's own attributes,
+    but on the set of detectors given to it.
+    """
+    def __init__(self, detectors: dict, evaluator: str, columns: dict = None):
+        """
+        Args:
+            detectors (dict): Set of detectors in ensemble. Should be keyed by
+                unique strings for convenient lookup, and valued by initialized
+                detector objects.
+            evaluator (str): String identifier for voting scheme by which to
+                determine if drift is present. E.g., 'simple-majority' uses
+                a function to determine if a simple majority of detectors
+                found drift. See options in ``menelaus.ensemble.evaluators``.
+            columns (dict, optional): Optional table of column lists to use
+                for each detector. Should be keyed to match the format of
+                ``detectors``. Will be used to filter the data columns passed
+                to ensemble in ``update`` according to each detector.
+        """
+        StreamingDetector.__init__(self)
+        Ensemble.__init__(self, detectors, evaluator, columns)
+
+    def update(self, X, y_true, y_pred):
+        """
+        Update ensemble itself, and each constituent detector with new data.
+        Calls ``Ensemble.update`` and ``StreamingDetector.update`` to do so.
+
+        Args:
+            X (numpy.ndarray): input data
+            y_true (numpy.ndarray): if applicable, true labels of input data
+            y_pred (numpy.ndarray): if applicable, predicted labels of input data
+        """
+        Ensemble.update(self, X=X, y_true=y_true, y_pred=y_pred)
+        StreamingDetector.update(self, X=X, y_true=y_true, y_pred=y_pred)
+
+    def reset(self):
+        """
+        Reset ensemble itself, and each constituent detector's drift state
+        and other relevant attributes. Intended for use after ``drift_state == 'drift'``.
+        Calls ``Ensemble.reset`` and ``StreamingDetector.reset`` to do so.
+        """
+        Ensemble.reset(self)
+        StreamingDetector.reset(self)
+
+
 class BatchEnsemble(BatchDetector, Ensemble):
     """
     Implements Ensemble class for batch-based drift detectors. Inherits
@@ -127,7 +159,6 @@ class BatchEnsemble(BatchDetector, Ensemble):
     update, and reset. Only internally, these operate not only on the
     ensemble's own attributes, but on the set of detectors given to it.
     """
-
     def __init__(self, detectors: dict, evaluator: str, columns: dict = None):
         """
         Args:
@@ -179,4 +210,10 @@ class BatchEnsemble(BatchDetector, Ensemble):
             y_true (numpy.array): actual labels of dataset
             y_pred (numpy.array): predicted labels of dataset
         """
-        Ensemble.set_reference(self, X=X, y_true=y_true, y_pred=y_pred)
+        for det_key in self.detectors:
+            # XXX - Cannot re-define X = constrain(), else external reference is modified
+            #       Need to see why this is happening and where to put e.g. a copy() stmt.
+            X_selected = self.select_data(X, det_key)
+            self.detectors[det_key].set_reference(
+                X=X_selected, y_true=y_true, y_pred=y_pred
+            )
