0322de4c4bc7ac2a4fc636a651da664738bbdc32
Thomas Schill <33845624+tms-bananaquit@users.noreply.github.com>
2022-10-25 17:21:23 -0400

    change drift threshold computation - the current implementation uses a random permutation for both new sets - which means they are not mutually exclusive. This means the numerator in compute_nnps_distance can contain 0's. - making the second set the reverse of the first fixes this.

--- src/menelaus/data_drift/nndvi.py
+++ src/menelaus/data_drift/nndvi.py
@@ -19,7 +19,7 @@ class NNDVI(DriftDetector):
     are compared against a threshold distance value, which is found
     by randomly sampling new reference and test sections, then fitting
     a Gaussian distribution to distance changes for those trials.
-    
+
     Attributes:
         total_samples (int): number of batches the drift detector has ever
             been updated with.
@@ -31,14 +31,10 @@ class NNDVI(DriftDetector):
         sampling_times (int): number of times to perform sampling for threshold estimation
         alpha (float): significance level for detecting drift
     """
-    input_type = 'batch'
 
-    def __init__(
-        self,
-        k_nn: int = 30,
-        sampling_times: int = 500,
-        alpha: float = 0.01
-    ):
+    input_type = "batch"
+
+    def __init__(self, k_nn: int = 30, sampling_times: int = 500, alpha: float = 0.01):
         """
         Attributes:
             k_nn (int, optional): the 'k' in k-Nearest-Neighbor (k-NN) search. Default 30.
@@ -59,29 +55,31 @@ class NNDVI(DriftDetector):
         Args:
           test_batch (numpy.array): next batch of data to detect drift on.
         """
-        if self._drift_state == 'drift':
+        if self._drift_state == "drift":
             self.reset()
 
         super().update()
         test_batch = np.array(test_batch)
         assert test_batch.shape[1] == self.reference_batch.shape[1]
-        
+
         # XXX - May want to do further checks about preserved order - Anmol
         nnsp = NNSpacePartitioner(self.k_nn)
         nnsp.build(self.reference_batch, test_batch)
         M_nnps = nnsp.nnps_matrix
-        v_ref, v_test = nnsp.v1, nnsp.v2 
+        v_ref, v_test = nnsp.v1, nnsp.v2
         d_act = NNSpacePartitioner.compute_nnps_distance(M_nnps, v_ref, v_test)
 
-        theta_drift = self._compute_drift_threshold(M_nnps, v_ref, v_test, self.sampling_times, self.alpha)
+        theta_drift = self._compute_drift_threshold(
+            M_nnps, v_ref, v_test, self.sampling_times, self.alpha
+        )
         if d_act > theta_drift:
-            self._drift_state = 'drift'
+            self._drift_state = "drift"
             self.set_reference(test_batch)
 
     def set_reference(self, new_reference_batch):
         """
         Set the detector's reference batch to an updated value; typically
-        used in ``update``. 
+        used in ``update``.
 
         Attributes:
             new_reference_batch (numpy.array): updated reference batch
@@ -99,7 +97,7 @@ class NNDVI(DriftDetector):
     @staticmethod
     def _compute_drift_threshold(M_nnps, v_ref, v_test, sampling_times, alpha):
         """
-        Compute critical value for drift detection, by repeatedly sampling 
+        Compute critical value for drift detection, by repeatedly sampling
         NNPS distance calculations and estimating a normal distribution from
         the same. Takes an NNPS matrix, indices of reference/test sections in
         the same, number of times to sample, and a significance level for which
@@ -119,13 +117,15 @@ class NNDVI(DriftDetector):
         d_shuffle = []
         for _ in range(sampling_times):
             # XXX - What does this mean? Especially keeping M the same, but shuffling Vs? - Anmol
-            v1_shuffle = np.random.permutation(v_ref) 
-            v2_shuffle = np.random.permutation(v_test)
-            d_i_shuffle = NNSpacePartitioner.compute_nnps_distance(M_nnps, v1_shuffle, v2_shuffle)
+            v1_shuffle = np.random.permutation(v_ref)
+            v2_shuffle = 1 - v1_shuffle
+
+            d_i_shuffle = NNSpacePartitioner.compute_nnps_distance(
+                M_nnps, v1_shuffle, v2_shuffle
+            )
             d_shuffle.append(d_i_shuffle)
         mu, std = norm.fit(d_shuffle)
         if mu == 0.0 and std == 0.0:
-            warnings.simplefilter('ignore', category=RuntimeWarning)
-        drift_threshold = norm.ppf(1-alpha, mu, std)
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+        drift_threshold = norm.ppf(1 - alpha, mu, std)
         return drift_threshold
-
