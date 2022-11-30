ff440a8ad34b2a67e9fb3f6b74be45a33fce52a8
Anmol-Srivastava <anmol.srivastava.ca@gmail.com>
2022-11-30 16:27:59 -0500

    add class swap function, minor doc fixes

--- menelaus/injection/class_manipulation.py
+++ menelaus/injection/class_manipulation.py
@@ -1,8 +1,41 @@
+import numpy as np
+import pandas as pd
 
 
+def class_swap(data, target_col, class_1, class_2, from_index, to_index):
+    """
+    Swaps two classes in a target column of a given dataset with each other.
+    One of the basic drift injection methods suggested in [CITE!].
+    Accepts ``pandas.DataFrame`` with column names or ``numpy.ndarray``
+    with column indices.
 
-def class_swap():
-    raise NotImplemented
+    Args:
+        data (np.array): data to inject with drift
+        target_col (int or str): column index/label of targets column
+        class_1 (int): value of first label in class swap
+        class_2 (int): value of second label in class swap
+        from_index: row index at which to start class swap
+        to_index: row index at which to end (non-inclusive) class swap
+
+    Returns:
+        np.array or pd.DataFrame: copy of data, with two classes swapped
+            in given target column, over given indices
+    """
+    if not isinstance(data, pd.DataFrame) and not isinstance(data, np.ndarray):
+        raise ValueError(f"Data of type {type(data)} not supported")    
+    elif isinstance(data, pd.DataFrame):  
+        ret = data.copy()
+        class_1_idx = ret[target_col] == class_1
+        class_2_idx = ret[target_col] == class_2
+        ret.loc[class_1_idx, target_col] = class_2
+        ret.loc[class_2_idx, target_col] = class_1
+    else:               
+        ret = np.copy(data)
+        class_1_idx = np.where(ret[target_col] == class_1)
+        class_2_idx = np.where(ret[target_col] == class_2)
+        ret[class_1_idx, target_col] = class_2
+        ret[class_2_idx, target_col] = class_1
+    return ret
 
 def class_join():
     raise NotImplemented
--- menelaus/injection/feature_manipulation.py
+++ menelaus/injection/feature_manipulation.py
@@ -6,13 +6,15 @@ def feature_swap(data, col_1, col_2, from_index, to_index):
     """
     Swaps two features/columns of a given dataset with each other.
     One of the basic drift injection methods suggested in [CITE!].
+    Accepts ``pandas.DataFrame`` with column names or ``numpy.ndarray``
+    with column indices.
 
     Args:
         data (np.array): data to inject with drift
-        col_1 (int): column index of first column
-        col_2 (int): column index of second column
-        from_index: row index at which to start column swap
-        to_index: row index at which to end (non-inclusive) column swap
+        col_1 (int or str): column index/label of first column
+        col_2 (int or str): column index/label of second column
+        from_index (int): row index at which to start column swap
+        to_index (int): row index at which to end (non-inclusive) column swap
 
     Returns:
         np.array or pd.DataFrame: copy of data, with two columns swapped
