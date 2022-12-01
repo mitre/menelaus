8a15176e2d5882ec7859b28ad60c98da14e9124f
Anmol-Srivastava <anmol.srivastava.ca@gmail.com>
2022-12-01 10:25:38 -0500

    finish basic class manipulation functions with some work TBD

--- menelaus/injection/class_manipulation.py
+++ menelaus/injection/class_manipulation.py
@@ -25,17 +25,71 @@ def class_swap(data, target_col, class_1, class_2, from_index, to_index):
         raise ValueError(f"Data of type {type(data)} not supported")    
     elif isinstance(data, pd.DataFrame):  
         ret = data.copy()
-        class_1_idx = ret[target_col] == class_1
-        class_2_idx = ret[target_col] == class_2
+        class_1_idx = ret.loc[
+            (ret.index in range(from_index, to_index)) & 
+            (ret[target_col] == class_1)
+        ].index
+        class_2_idx = ret.loc[
+            (ret.index in range(from_index, to_index)) &
+            (ret[target_col] == class_2)
+        ]
         ret.loc[class_1_idx, target_col] = class_2
         ret.loc[class_2_idx, target_col] = class_1
     else:               
         ret = np.copy(data)
-        class_1_idx = np.where(ret[target_col] == class_1)
-        class_2_idx = np.where(ret[target_col] == class_2)
+        # np.where returns tuples pertaining to dimensions, only need the row
+        class_1_idx, _ = np.where(ret[:, target_col] == class_1)
+        class_1_idx = class_1_idx[class_1_idx in range(from_index, to_index)]
+        class_2_idx, _ = np.where(ret[:, target_col] == class_2)
+        class_2_idx = class_2_idx[class_2_idx in range(from_index, to_index)]
         ret[class_1_idx, target_col] = class_2
         ret[class_2_idx, target_col] = class_1
     return ret
 
-def class_join():
-    raise NotImplemented
+def class_join(
+    data,
+    target_col,
+    class_1,
+    class_2,
+    new_class,
+    from_index,
+    to_index
+):
+    """
+    Joins two [TODO or more?] classes in a unique class. One of the basic
+    drift injection methods suggested in (Vreeken et al., 2007).
+    Accepts ``pandas.DataFrame`` with column names or ``numpy.ndarray``
+    with column indices.
+
+    Args:
+        data (np.array): data to inject with drift
+        target_col (int or str): column index/label of targets column
+        class_1 (int): value of first label in class join
+        class_2 (int): value of second label in class join,
+        new_class (int): new label value to assign to old classes
+        from_index: row index at which to start class join
+        to_index: row index at which to end (non-inclusive) class join
+
+    Returns:
+        np.array or pd.DataFrame: copy of data, with two classes joined
+            in given target column, over given indices, into new class
+    """
+    if not isinstance(data, pd.DataFrame) and not isinstance(data, np.ndarray):
+        raise ValueError(f"Data of type {type(data)} not supported")    
+    elif isinstance(data, pd.DataFrame):  
+        ret = data.copy()
+        class_idx = ret.loc[
+            (ret.index in range(from_index, to_index)) & 
+            ((ret[target_col] == class_1) | (ret[target_col] == class_2))
+        ].index
+        ret.loc[class_idx, target_col] = new_class
+    else:               
+        ret = np.copy(data)
+        # np.where returns tuples pertaining to dimensions, only need the row
+        class_idx, _ = np.where(
+            (ret[:, target_col] == class_1) | 
+            (ret[:, target_col] == class_2)
+        )
+        class_idx = class_idx[class_idx in range(from_index, to_index)]
+        ret[class_idx, target_col] = new_class
+    return ret
