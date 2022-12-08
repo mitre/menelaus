7ce5ed7ada3c78deb3ee8ba867cf84f85b7cbce0
Anmol-Srivastava <anmol.srivastava.ca@gmail.com>
2022-12-08 11:00:02 -0500

    add brownian noise draft

--- menelaus/injection/__init__.py
+++ menelaus/injection/__init__.py
@@ -1,5 +1,5 @@
 """This module contains functions with which to artificially inject drift into a dataset."""
 
-from menelaus.injection.feature_manipulation import feature_swap, feature_hide_and_reorder
+from menelaus.injection.feature_manipulation import feature_swap, feature_hide_and_sample
 from menelaus.injection.class_manipulation import class_swap, class_join
-from menelaus.injection.noise import _
+from menelaus.injection.noise import brownian_noise
