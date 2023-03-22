17c65199d30ee4e88869f5c31e5f6e40a8eb2b62
Anmol-Srivastava <anmol.srivastava.ca@gmail.com>
2023-03-22 16:05:14 -0400

    format top-level init with black

--- menelaus/__init__.py
+++ menelaus/__init__.py
@@ -1,7 +1,7 @@
 import importlib.metadata
 
 # version from setup.cfg
-__version__ = importlib.metadata.version('menelaus')
+__version__ = importlib.metadata.version("menelaus")
 
 # name of environment variable we use to check for test harness mode
 test_env_var = "MENELAUS_TEST_MODE"
