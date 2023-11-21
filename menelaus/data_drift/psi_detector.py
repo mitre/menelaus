395d84003d5c0ee2d466b348ca5ec438189b68ba
Tim Chen <115333718+951378644@users.noreply.github.com>
2023-11-21 18:28:24 -0500

    Update psi_detector.py

--- menelaus/data_drift/psi_detector.py
+++ menelaus/data_drift/psi_detector.py
@@ -1,128 +1,143 @@
 from menelaus.detector import BatchDetector
 import pandas as pd
 import numpy as np
-import copy
-
-
 class PSI_Detector(BatchDetector):
+    """
+    Parent class for PSI-based drift detector, it serves as a fundamental framework for batch data applications.
+    
+    The PSI (Population Stability Index) is employed for detecting distributional shifts between a reference population
+    and a comparison population. This detector assesses changes by calculating the PSI, which measures the distributional
+    change based on percentiles. The psi function in the detector compares the distributions of scores in reference and 
+    test populations and calculates the PSI values for different bins.
+    
+    In summary, the PSI drift detector provides a robust mechanism for monitoring and detecting distributional changes in 
+    populations, making it adaptable for various data settings and applications.
+    
+    Ref. :cite:t:`Psi2022`
+    """
     input_type = "batch"
 
     def __init__(
         self,
+        eps=1e-4,
+        threshold=0.1
     ):
-        # This initializes batch detector's parent class
-        super().__init__()
+        """
+        Args:
+            eps:The eps parameter in the function represents a small constant (1e-4) introduced to prevent division by zero 
+                when calculating percentages, ensuring numerical stability.
+            threshold: It represents the threshold for detecting drift, if the calculated PSI value for a feature exceeds 
+                this threshold, it indicates drift in that feature, and the drift_state is set to 'drift'. This threshold is a 
+                user-defined value, and when crossed, it signifies a significant distributional change between the reference 
+                and test populations.
+        """
+        super().__init__() 
+        self.eps = eps
+        self.threshold = threshold
 
-        # Initialize any parameters to be used in this algorithm
 
     def set_reference(self, X, y_true=None, y_pred=None):
-        # leave this, it uses the parent class to validate input
-        # and sets the self.reference variable to refer to the reference dataset
+        """
+        Set the detector's reference batch to an updated value; typically
+        used in ``update``.
 
+        Attributes:
+            X (numpy.array): updated reference batch
+            y_true (numpy.array): true labels, not used in NNDVI
+            y_pred (numpy.array): predicted labels, not used in NNDVI
+        """
         X, _, _ = super()._validate_input(X, None, None)
-        X = pd.DataFrame(X, columns=self._input_cols)
-
-        # Initialize reference dataset
-        self.reference = copy.deepcopy(X)
-
-        self.reset()
-
+        self.reference = X.reshape(len(X),)
+        
+        
     def reset(self):
+        """
+        Initialize relevant attributes to original values, to ensure information
+        only stored from samples_since_reset onwards. Intended for use
+        after ``drift_state == 'drift'``.
+        """
         super().reset()
 
-    def update(self, X, by_feature=True, X_by_feature=None, y_true=None, y_pred=None):
-        # this function will update the detector with the test batch
-        eps = 1e-4
-        # create a variable to store psi values for each feature
-        feature_psi = []
-        o = pd.DataFrame(
-            columns=[
-                "Column",
-                "Moderate population change",
-                "Significant population change",
-                "PSI",
-            ]
-        )
-        z = 0
-        # 1. iterate through each feature in reference and test data, identify minimum and maximum value
-        for column in self.reference.columns:
-            min_val = min(min(self.reference[column]), min(X[column]))
-            max_val = max(max(self.reference[column]), max(X[column]))
+        
+    def update(self, X: np.array, y_true=None, y_pred=None):
+        """
+        Update the detector with a new test batch. If drift is detected, new
+        reference batch becomes most recent test batch.
 
-            # 2. use _bin_data function to bucketize reference, append to reference buckets array
-            bins = self._bin_data(self.reference[column], min_val, max_val)
-            bins_initial = pd.cut(
-                self.reference[column], bins=bins, labels=range(1, len(bins))
-            )
-            df_initial = pd.DataFrame(
-                {"initial": self.reference[column], "bin": bins_initial}
-            )
-            grp_initial = df_initial.groupby("bin").count()
-            grp_initial["percent_initial"] = grp_initial["initial"] / sum(
-                grp_initial["initial"]
-            )
-            # 3. use _bin_data function to bucketize test, append to reference test array
-            bins_new = pd.cut(X[column], bins=bins, labels=range(1, len(bins)))
-            df_new = pd.DataFrame({"new": X[column], "bin": bins_new})
-            grp_new = df_new.groupby("bin").count()
-            grp_new["percent_new"] = grp_new["new"] / sum(grp_new["new"])
-            # 4. Call PSI function to calculate PSI on test and reference bucket representation,
-            psi_value = self._PSI(grp_initial, grp_new)
-            feature_psi.append([column, psi_value])
-            # store PSI for each feature in feature_psi array
-            if psi_value >= 0.1 and psi_value <= 0.2:
-                o.loc[z] = [column, "Yes", "No", psi_value]
-                z += 1
-            elif psi_value > 0.2:
-                o.loc[z] = [column, "No", "Yes", psi_value]
-                z += 1
-        # 5. Aggregate PSI values to determine if dataset is drifting
-        if o.any()["Column"] == True:
-            self.drift_state == "drift"
-            return feature_psi, o
-        else:
-            return "no drift detected", feature_psi
-        # If PSI indicates drift, set self.drift_state == 'drift'
+        Args:
+          X (numpy.array): next batch of data to detect drift on.
+          y_true (numpy.array): true labels, not used in PSI
+          y_pred (numpy.array): predicted labels, not used in PSI
+        """ 
+        if self._drift_state == "drift":
+            self.reset()
 
-        # Create a dictionary to store if each individual feature is drifting
+        X, _, _ = super()._validate_input(X, None, None)
 
-        # Update self.reference dataset to refer to test data, X
-        self.reference = X
+        super().update(X=X, y_true=None, y_pred=None)
+        test_batch = (np.array(X)).reshape(len(X),)
+        min_val = min(min(self.reference), min(test_batch))
+        max_val = max(max(self.reference), max(test_batch))
+        bins = self._bin_data(self.reference,min_val,max_val)
+        bins_initial = pd.cut(self.reference, bins = bins, labels = range(1,len(bins)))
+        df_initial = pd.DataFrame({'initial': self.reference, 'bin': bins_initial})
+        grp_initial = df_initial.groupby('bin').count()
+        grp_initial['percent_initial'] = grp_initial['initial'] / sum(grp_initial['initial'])
+        bins_new = pd.cut(test_batch, bins = bins, labels = range(1,len(bins)))
+        df_new = pd.DataFrame({'new': test_batch, 'bin': bins_new})
+        grp_new = df_new.groupby('bin').count()
+        grp_new['percent_new'] = grp_new['new'] / sum(grp_new['new'])
+        psi_value = self._PSI(grp_initial,grp_new)
+        self.PSI_value = psi_value
+        if psi_value >= self.threshold:
+            self._drift_state = "drift"
+            self.set_reference(test_batch)
 
     def _bin_data(self, feature, min, max):
-        eps = 1e-4
-        if len(feature.unique()) < 10:
-            bins = [
-                min + (max - min) * (i) / len(feature.unique())
-                for i in range(len(feature.unique()) + 1)
-            ]
-            bins[0] = min - eps  # Correct the lower boundary
-            bins[-1] = max + eps  # Correct the higher boundary
+        """
+        Bin the given feature based on the specified minimum and maximum values.
+
+        Args:
+            feature (numpy.array): The feature to be binned.
+            min (float): The minimum value for binning.
+            max (float): The maximum value for binning.
+
+        Returns:
+            list: A list of bin edges for the given feature.
+        """
+        if len(np.unique(feature)) < 10:
+            bins = [min + (max - min)*(i)/len(np.unique(feature)) for i in range(len(np.unique(feature))+1)]
+            bins[0] = min - self.eps
+            bins[-1] = max + self.eps
             return bins
         else:
-            bins = [min + (max - min) * (i) / 10 for i in range(10 + 1)]
-            bins[0] = min - eps  # Correct the lower boundary
-            bins[-1] = max + eps  # Correct the higher boundary
+            bins = [min + (max - min)*(i)/10 for i in range(10+1)]
+            bins[0] = min - self.eps
+            bins[-1] = max + self.eps
             return bins
-            # return an array containing the sample counts within each bucket
+
 
     def _PSI(self, reference_feature, test_feature):
-        eps = 1e-4
-        # Compare the bins to calculate PSI
-        psi_df = reference_feature.join(test_feature, on="bin", how="inner")
+        """
+        Calculate the Population Stability Index (PSI) between reference and test features.
 
-        # Add a small value for when the percent is zero
-        psi_df["percent_initial"] = psi_df["percent_initial"].apply(
-            lambda x: eps if x == 0 else x
-        )
-        psi_df["percent_new"] = psi_df["percent_new"].apply(
-            lambda x: eps if x == 0 else x
-        )
+        Args:
+            reference_feature (pandas.DataFrame): Reference feature distribution.
+            test_feature (pandas.DataFrame): Test feature distribution.
 
-        # Calculate the psi
-        psi_df["psi"] = (psi_df["percent_initial"] - psi_df["percent_new"]) * np.log(
-            psi_df["percent_initial"] / psi_df["percent_new"]
-        )
+        Returns:
+            float: The calculated PSI value indicating distributional change.
+        """
+        psi_df = reference_feature.join(test_feature, on = "bin", how = "inner")
+        psi_df['percent_initial'] = psi_df['percent_initial'].apply(lambda x: self.eps if x == 0 else x)
+        psi_df['percent_new'] = psi_df['percent_new'].apply(lambda x: self.eps if x == 0 else x)
+        psi_df['psi'] = (psi_df['percent_initial'] - psi_df['percent_new']) * np.log(psi_df['percent_initial'] / psi_df['percent_new'])
+        return np.mean(psi_df['psi'])
+    def PSI_value(self):
+        """
+        Get the last calculated PSI value.
 
-        # Return the mean of psi values
-        return np.mean(psi_df["psi"])
+        Returns:
+            float: The PSI value from the most recent update.
+        """
+        return self.PSI_value
