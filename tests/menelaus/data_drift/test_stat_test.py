666cc311852b249e151a9a62cda97fcad835e8b6
Tim Chen <115333718+951378644@users.noreply.github.com>
2023-12-20 13:33:43 -0500

    Update test_stat_test.py

--- tests/menelaus/data_drift/test_stat_test.py
+++ tests/menelaus/data_drift/test_stat_test.py
@@ -1,12 +1,170 @@
 import pytest
 from stat_test import GenericDetector, CHIDetector, KSDetector, CVMDetector, FETDetector
 import numpy as np
+import pandas as pd
 
-def test_generic_detector_initialization():
-    # Test initialization of GenericDetector
-    representation = MockRepresentation()
-    divergence = MockDivergence()
-    crit_function = MockCritFunction()
-    detector = GenericDetector(representation, divergence, crit_function)
-    assert detector is not None
+class MockRepresentation:
+    def fit(self, X):
+        # Simple mock behavior, maybe just return the input for simplicity
+        return X
+
+
+def MockDivergence(rep_ref, rep_test):
+        # A simple mock that returns a fixed value or a simple calculation
+        return sum(rep_ref) - sum(rep_test)
+
+
+def MockCritFunction(divergence, alpha = 0.05):
+        # Mock behavior that returns True or False based on a simple condition
+        return divergence > alpha
+
+class TestGenericDetector:
+    def test_initialization(self):
+        detector = GenericDetector(representation=MockRepresentation(), 
+                      divergence=MockDivergence, 
+                      crit_function=MockCritFunction)
+        assert detector is not None
+
+    def test_set_reference(self):
+        detector = GenericDetector(representation=MockRepresentation(), 
+                      divergence=MockDivergence, 
+                      crit_function=MockCritFunction)
+        reference_data = np.array([1, 2, 3])
+        detector.set_reference(reference_data)
+        assert np.array_equal(detector.reference, reference_data)
+
+    def test_update_no_drift(self):
+        detector = GenericDetector(representation=MockRepresentation(), 
+                      divergence=MockDivergence, 
+                      crit_function=MockCritFunction)
+        reference_data = np.array([1, 2, 3])
+        new_data = np.array([1, 2, 3])
+        detector.set_reference(reference_data)
+        detector.update(new_data)
+        assert detector.drift_state is None  # No drift expected
+
+    def test_update_with_drift(self):
+        detector = GenericDetector(representation=MockRepresentation(), 
+                      divergence=MockDivergence, 
+                      crit_function=MockCritFunction)
+        reference_data = np.array([10, 20, 30])
+        new_data = np.array([1, 2, 3])
+        detector.set_reference(reference_data)
+        detector.update(new_data)
+        assert detector.drift_state is not None  #drift expected
     
+class TestCHIDetector:
+    def test_initialization(self):
+        # Assuming CHIDetector has a default initialization without parameters
+        detector = CHIDetector()
+        assert detector is not None
+
+    def test_set_reference(self):
+        detector = CHIDetector()
+        reference_data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        with pytest.raises(ValueError):
+            detector.set_reference(reference_data)
+
+    def test_update_no_drift(self):
+        detector = CHIDetector()
+        reference_data = np.array([1, 2, 3])
+        new_data = np.array([1, 2, 3])
+        detector.set_reference(reference_data)
+        detector.update(new_data)
+        assert detector.drift_state is None  # No drift expected
+
+        
+    def test_update_with_drift(self):
+        detector = CHIDetector()
+        reference_data = np.array(['OM','OO','OK'])
+        new_data = np.array(['OK','OK','OK','OK','OK','OK','OK','OK'])
+        detector.set_reference(reference_data)
+        detector.update(new_data)
+        assert detector.drift_state is not None
+
+class TestKSDetector:
+    def test_initialization(self):
+        # Assuming KSDetector has a default initialization without parameters
+        detector = KSDetector()
+        assert detector is not None
+
+    def test_set_reference(self):
+        detector = KSDetector()
+        reference_data = np.array(['OM','OO','OK'])
+        with pytest.raises(ValueError):
+            detector.set_reference(reference_data)
+
+    def test_update_no_drift(self):
+        detector = KSDetector()
+        reference_data = np.array([1, 2, 3])
+        new_data = np.array([1, 2, 3])
+        detector.set_reference(reference_data)
+        detector.update(new_data)
+        assert detector.drift_state is None  # No drift expected
+
+        
+    def test_update_with_drift(self):
+        detector = KSDetector()
+        reference_data = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+        new_data = np.array([200,300,400])
+        detector.set_reference(reference_data)
+        detector.update(new_data)
+        assert detector.drift_state is not None
+        
+class TestCVMDetector:
+    def test_initialization(self):
+        # Assuming CVMDetector has a default initialization without parameters
+        detector = CVMDetector()
+        assert detector is not None
+
+    def test_set_reference(self):
+        detector = CVMDetector()
+        reference_data = np.array(['OM','OO','OK'])
+        with pytest.raises(ValueError):
+            detector.set_reference(reference_data)
+
+    def test_update_no_drift(self):
+        detector = CVMDetector()
+        reference_data = np.array([1, 2, 3])
+        new_data = np.array([1, 2, 3])
+        detector.set_reference(reference_data)
+        detector.update(new_data)
+        assert detector.drift_state is None  # No drift expected
+
+        
+    def test_update_with_drift(self):
+        detector = CVMDetector()
+        reference_data = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+        new_data = np.array([200,300,400])
+        detector.set_reference(reference_data)
+        detector.update(new_data)
+        assert detector.drift_state is not None
+
+class TestFETDetector:
+    def test_initialization(self):
+        # Assuming FETDetector has a default initialization without parameters
+        detector = FETDetector()
+        assert detector is not None
+
+    def test_set_reference(self):
+        detector = FETDetector()
+        reference_data = np.array(['OM','OO','OK'])
+        with pytest.raises(ValueError):
+            detector.set_reference(reference_data)
+
+    def test_update_no_drift(self):
+        detector = FETDetector()
+        reference_data = np.array([1,1,1,1,1,0,0,0,0,0])
+        new_data = np.array([1,0])
+        detector.set_reference(reference_data)
+        detector.update(new_data,alt = 'less')
+        assert detector.drift_state is None  # No drift expected
+
+        
+    def test_update_with_drift(self):
+        detector = FETDetector()
+        reference_data = np.array([1,1,1,1,1,0,0,0,0,0])
+        new_data = np.array([1,0,0,0,0,0,0,0,0,0,0,0,0])
+        detector.set_reference(reference_data)
+        detector.update(new_data,alt = 'two-sided')
+        assert detector.drift_state is not None
