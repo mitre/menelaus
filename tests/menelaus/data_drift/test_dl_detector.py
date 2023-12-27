d782093d66b690ae29c705c07bfcc08434fb8a15
Tim Chen <115333718+951378644@users.noreply.github.com>
2023-12-27 15:59:24 -0500

    Black

--- tests/menelaus/data_drift/test_dl_detector.py
+++ tests/menelaus/data_drift/test_dl_detector.py
@@ -4,6 +4,7 @@ import pytest
 from menelaus.data_drift import dl_detector
 from sklearn.preprocessing import StandardScaler
 
+
 def test_dl_detector_init():
     """Test correct default initialization for dl_detector"""
     det = DL_Detector()
@@ -13,48 +14,72 @@ def test_dl_detector_init():
     assert det.alpha == 0.05
     assert det.threshold == 0.5
 
+
 def test_dl_detector_set_reference():
     """Assert dl_detector.set_reference works as intended"""
     det = DL_Detector()
     scaler = StandardScaler()
-    ref = pd.DataFrame(np.ones((rows, columns)), columns=[f'Column_{i}' for i in range(columns)])
+    ref = pd.DataFrame(
+        np.ones((rows, columns)), columns=[f"Column_{i}" for i in range(columns)]
+    )
     det.set_reference(ref)
-    assert det.reference == pd.DataFrame(scaler.fit_transform(ref), columns = ref.columns, index = ref.index)
+    assert det.reference == pd.DataFrame(
+        scaler.fit_transform(ref), columns=ref.columns, index=ref.index
+    )
+
 
 def test_dl_detector_update_1():
     """Ensure dl_detector can return no drift accurately"""
     det = DL_Detector()
-    ref = pd.DataFrame(np.ones((rows, columns)), columns=[f'Column_{i}' for i in range(columns)])
+    ref = pd.DataFrame(
+        np.ones((rows, columns)), columns=[f"Column_{i}" for i in range(columns)]
+    )
     det.set_reference(ref)
-    det.update(ref.replace(1,2))
+    det.update(ref.replace(1, 2))
     assert det.drift_state is None
+
 
 def test_dl_detector_update_2():
     """Ensure dl_detector can update with drift actions triggered"""
     det = DL_Detector()
-    ref = pd.DataFrame(np.ones((rows, columns)), columns=[f'Column_{i}' for i in range(columns)])
-    test = pd.DataFrame(np.random.randint(0, 100, size=(rows, columns)), columns=[f'Column_{i}' for i in range(columns)])
+    ref = pd.DataFrame(
+        np.ones((rows, columns)), columns=[f"Column_{i}" for i in range(columns)]
+    )
+    test = pd.DataFrame(
+        np.random.randint(0, 100, size=(rows, columns)),
+        columns=[f"Column_{i}" for i in range(columns)],
+    )
     det.set_reference(ref)
     det.update(test)
     assert det.drift_state is not None
 
+
 def test_dl_detector_update_3():
     """Ensure dl_detector can update with drift actions triggered when by_feature equal False"""
     det = DL_Detector()
-    ref = pd.DataFrame(np.ones((rows, columns)), columns=[f'Column_{i}' for i in range(columns)])
-    test = pd.DataFrame(np.random.randint(0, 100, size=(rows, columns)), columns=[f'Column_{i}' for i in range(columns)])
+    ref = pd.DataFrame(
+        np.ones((rows, columns)), columns=[f"Column_{i}" for i in range(columns)]
+    )
+    test = pd.DataFrame(
+        np.random.randint(0, 100, size=(rows, columns)),
+        columns=[f"Column_{i}" for i in range(columns)],
+    )
     det.set_reference(ref)
-    det.update(test,by_feature = False)
+    det.update(test, by_feature=False)
     assert det.drift_state is not None
+
 
 def test_dl_detector_update_4():
     """Check dl_detector.update behavior after drift alarm"""
     det = DL_Detector()
-    ref = pd.DataFrame(np.ones((rows, columns)), columns=[f'Column_{i}' for i in range(columns)])
+    ref = pd.DataFrame(
+        np.ones((rows, columns)), columns=[f"Column_{i}" for i in range(columns)]
+    )
     det.set_reference(ref)
     det._drift_state = "drift"
-    det.update(ref.replace(1,2))
+    det.update(ref.replace(1, 2))
     assert det.drift_state is None
+
 
 def test_dl_detector_reset():
     """Check dl_detector.reset works as intended"""
